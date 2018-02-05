93a41499818ff4e755f7c749787390b9ed87060d
Oleg Nechiporenko <onechiporenko@apache.org>
2018-02-05 11:34:38 +0200

    Fix JSON.parse/serialize of `undefined`

--- app/transforms/object.js
+++ app/transforms/object.js
@@ -1,11 +1,12 @@
+import {typeOf} from '@ember/utils';
 import DS from 'ember-data';
 
 export default DS.Transform.extend({
   deserialize(serialized) {
-    return JSON.stringify(serialized, null, 2);
+    return typeOf(serialized) === 'undefined' ? serialized : JSON.stringify(serialized, null, 2);
   },
 
   serialize(deserialized) {
-    return JSON.parse(deserialized);
+    return typeOf(deserialized) === 'undefined' ? deserialized : JSON.parse(deserialized);
   }
 });
